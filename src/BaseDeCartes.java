5a93b86867eee5019e407db5cfe5a1a250bc034f
theoatechian <theo.atechian@utbm.fr>
2025-01-03 15:30:57 +0100

    modif critère pour problème 4 cartes

--- src/BaseDeCartes.java
+++ src/BaseDeCartes.java
@@ -7,19 +7,24 @@ public class BaseDeCartes {
 
     static {
         //creation des cartes criteres avec un id de hash map et des cartes avec leur texte et un id de critere
-        carte.put(1,new CarteCritere("<html> cm > td</html>",new Critere(1)));
-        carte.put(2,new CarteCritere("<html> cm + td > tp</html> ",new Critere(2)));
-        carte.put(3,new CarteCritere("<html> cm = td</html>",new Critere(3)));
-        carte.put(4,new CarteCritere("<html> cm + td ≤ 3 heures</html>",new Critere(4)));
-        carte.put(5,new CarteCritere("<html> cm + tp ≤ 3 heures</html>",new Critere(5)));
-        carte.put(6,new CarteCritere("<html> td + tp ≤ 3 heures</html> ",new Critere(6)));
-        carte.put(7, new CarteCritere("<html> cm + td + tp = 6 heures</html>", new Critere(7)));
-        carte.put(8, new CarteCritere("<html> cm + tp ≤ td</html>", new Critere(8)));
-        carte.put(9, new CarteCritere("<html> td + tp ≤ cm</html>", new Critere(9)));
-        carte.put(10, new CarteCritere("<html> cm ≤ td</html>", new Critere(10)));
-        carte.put(11, new CarteCritere("<html> cm > tp</html>", new Critere(11)));
-        carte.put(12, new CarteCritere("<html> td > tp</html>", new Critere(12)));
-    
+        carte.put(1,new CarteCritere("<html> cm = 1 | cm > 1</html>",new Critere(1)));
+        carte.put(2,new CarteCritere("<html> td ≤ tp | td = tp | td > tp</html> ",new Critere(2)));
+        carte.put(3,new CarteCritere("<html> cm + td ≤ 6h | cm + td = 6h | cm + td > 6h</html>",new Critere(3)));
+        carte.put(4,new CarteCritere("<html> cm ≤ 3h | cm = 3h | cm > 3h</html>",new Critere(4)));
+        carte.put(5,new CarteCritere("<html> cm ≤ tp | cm = tp | cm > tp</html>",new Critere(5)));
+        carte.put(6,new CarteCritere("<html> td ≤ cm + tp | td = cm + tp | td > cm + tp</html> ",new Critere(6)));
+        carte.put(7, new CarteCritere("<html> cm + td + tp = pair | cm + td + tp = impair</html>", new Critere(7)));
+        carte.put(8, new CarteCritere("<html> td ≤ 4h | td = 4h | td > 4h</html>", new Critere(8)));
+        carte.put(9, new CarteCritere("<html> tp = pair | tp = impair</html>", new Critere(9)));
+        carte.put(10, new CarteCritere("<html> td ≤ tp | td = tp | td > tp</html>", new Critere(10)));
+        carte.put(11, new CarteCritere("<html> cm ≤ td + tp | cm = td + tp | cm > td + tp</html>", new Critere(11)));
+        carte.put(12, new CarteCritere("<html> cm ≤ 3h | cm = 3h | cm > 3h</html>", new Critere(12)));
+        carte.put(13, new CarteCritere("<html> cm + td + tp ≤ 6h | cm + td + tp = 6h | cm + td + tp > 6h</html>", new Critere(13)));
+        carte.put(14, new CarteCritere("<html> td = pair | td = impair</html>", new Critere(14)));
+        carte.put(15, new CarteCritere("<html> td ≤ tp | td = tp | td > tp</html>", new Critere(15)));
+        carte.put(16, new CarteCritere("<html> tp ≤ 3h | tp = 3h | tp > 3h</html>", new Critere(16)));
+        carte.put(17, new CarteCritere("<html> tp ≤ cm | tp = cm | tp > cm</html>", new Critere(17)));
+        carte.put(18, new CarteCritere("<html> le chiffre 1 apparait une seule fois | le chiffre 1 apparait plus d'une fois<html>", new Critere(18)));
     }
     
     //getter pour recuperer une carte a partir d'un id
--- src/Critere.java
+++ src/Critere.java
@@ -15,19 +15,53 @@ public class Critere {
     //methode pour interroger un critere (change les condition en fonction de l'id du critere)
     public boolean verifier(int v1, int v2, int v3) {
         return switch (id) {
-            case 1 -> v1 > v2;
-            case 2 -> v1 + v2 > v3;
-            case 3 -> v1 == v2 && v2 == v3;
-            case 4 -> v1 + v2 < 3;
-            case 5 -> v1 + v3 < 3;
-            case 6 -> v2 + v3 < 3;
-            case 7 -> v1 + v2 + v3 == 6;
-            case 8 -> v1 + v3 < v2;
-            case 9 -> v2 + v3 < v1;
-            case 10 -> v1 < v2;
-            case 11-> v1 > v3;
-            case 12 -> v2 < v3;
+            case 1 -> v1 > 1;
+            case 2 -> v2 > v1;
+            case 3 -> v1 + v2 == 6;
+            case 4 -> v1 == 3;
+            case 5 -> v1 == v3 ;
+            case 6 -> v3 < v1 + v2;
+            case 7 -> (v1 + v2 + v3) % 2 != 0;
+            case 8 -> v2 == 4;
+            case 9 -> v3 % 2 == 0;
+            case 10 -> v2 > v3;
+            case 11-> v1 > v2 + v3;
+            case 12 -> v1 < 3;
+            case 13 -> v1 + v2 + v3 == 6;
+            case 14 -> v2 % 2 == 0;
+            case 15 -> v2 == v3;
+            case 16 -> v3 == 3;
+            case 17 -> v3 > v1;
+            case 18 -> {
+                // Vérification nombre "1" dans le code
+                String code = """
+                        case 1 -> v1 > 1;
+                        case 2 -> v2 > v1;
+                        case 3 -> v1 + v2 == 6;
+                        case 4 -> v1 == 3;
+                        case 5 -> v1 == v3 ;
+                        case 6 -> v3 < v1 + v2;
+                        case 7 -> (v1 + v2 + v3) % 2 != 0;
+                        case 8 -> v2 == 4;
+                        case 9 -> v3 % 2 == 0;
+                        case 10 -> v2 > v3;
+                        case 11-> v1 > v2 + v3;
+                        case 12 -> v1 < 3;
+                        case 13 -> v1 + v2 + v3 == 6;
+                        case 14 -> v2 % 2 == 0;
+                        case 15 -> v2 == v3;
+                        case 16 -> v3 == 3;
+                        case 17 -> v3 > v1;
+                        """;
+                    int oneCount = countOnes(code);
+                    yield oneCount == 1;
+            }
             default -> false;
         };
     }
+    // Méthode pour compter les occurrences du chiffre "1" dans une chaîne de caractères
+    private int countOnes(String codeSnippet) {
+        return (int) codeSnippet.chars().filter(ch -> ch == '1').count();
+    }
 }
+
